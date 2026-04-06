6e638ea7e76ef9937d3041cd5778ce6943b04043
ivan-s-2001 <ivan.s.2001@yandex.com>
2026-04-06 11:22:34 +0300

    feat: add picket character config

---
+++ src/content/characters/picket/character.js
@@ -0,0 +1,11 @@
+export default {
+  id: 'picket',
+  name: 'Пикет',
+  summary: 'Щит команды, который превращает защиту в контроль, урон и безопасность.',
+  pages: [
+    { id: 'overview', label: 'О герое' },
+    { id: 'dice', label: 'Кости' },
+    { id: 'tools', label: 'Помощники' },
+    { id: 'mechanics', label: 'Механики' },
+  ],
+}
