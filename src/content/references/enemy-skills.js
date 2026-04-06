df5cefbf2bb72a0b53ce938ec39851d388fe8ee9
ivan-s-2001 <ivan.s.2001@yandex.com>
2026-04-06 11:55:05 +0300

    fix: add enemy skills reference

---
+++ src/content/references/enemy-skills.js
@@ -0,0 +1,17 @@
+export default {
+  id: 'enemy-skills',
+  title: 'Навыки злодеев',
+  sections: [
+    {
+      id: 'intro',
+      label: 'Заглушка',
+      title: 'Справочник будет заполнен позже',
+      blocks: [
+        {
+          type: 'paragraph',
+          text: 'Эта reference-страница уже работает через ту же block-based систему, что и страницы героев.',
+        },
+      ],
+    },
+  ],
+}
