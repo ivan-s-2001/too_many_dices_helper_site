9a3bedb40fb32a8bb534d454e616cf7ab415b84b
ivan-s-2001 <ivan.s.2001@yandex.com>
2026-04-06 11:54:50 +0300

    fix: add references registry

---
+++ src/content/references/index.js
@@ -0,0 +1,9 @@
+import enemySkills from './enemy-skills'
+import slotsAndDice from './slots-and-dice'
+import endOfDay from './end-of-day'
+
+export default {
+  [enemySkills.id]: enemySkills,
+  [slotsAndDice.id]: slotsAndDice,
+  [endOfDay.id]: endOfDay,
+}
