c4c26e63faa16da249ee2a0855b498e39395c1f1
ivan-s-2001 <ivan.s.2001@yandex.com>
2026-04-06 11:35:35 +0300

    feat: add block registry

---
+++ src/components/renderers/blockRegistry.js
@@ -0,0 +1,19 @@
+import ParagraphBlock from '../blocks/ParagraphBlock'
+import BulletListBlock from '../blocks/BulletListBlock'
+import CalloutBlock from '../blocks/CalloutBlock'
+import CardsBlock from '../blocks/CardsBlock'
+import ChecklistBlock from '../blocks/ChecklistBlock'
+import RouteStepsBlock from '../blocks/RouteStepsBlock'
+import TrackerBlock from '../blocks/TrackerBlock'
+import StatGridBlock from '../blocks/StatGridBlock'
+
+export default {
+  paragraph: ParagraphBlock,
+  bulletList: BulletListBlock,
+  callout: CalloutBlock,
+  cards: CardsBlock,
+  checklist: ChecklistBlock,
+  routeSteps: RouteStepsBlock,
+  tracker: TrackerBlock,
+  statGrid: StatGridBlock,
+}
